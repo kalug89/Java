1b602c4aae8ae5e266486427fcd5d8f7360aa1f4
kalug89 <kalug89>
2019-09-09 10:14:48 +0200

    Add two number program

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+     /*1 Add two Numbers */
+
+//        int firstNumber = 1;
+//        int secondNumber = 2;
+//
+//        int sum = firstNumber + secondNumber;
+//
+//        System.out.println(firstNumber + secondNumber);
+//        System.out.println(sum);
+
+     /*2 Add two Numbers with input number */
+
+//        System.out.println("Add two number. Please give me first:");
+//        Scanner scannerFirstNumber = new Scanner(System.in);
+//        int firstNum = scannerFirstNumber.nextInt();
+//
+//        System.out.println("and second");
+//        Scanner scannerSecondNumber = new Scanner(System.in);
+//        int secondNum = scannerSecondNumber.nextInt();
+//
+//        int scannerSum = firstNum + secondNum;
+//        System.out.println("Sum is: " + scannerSum);
+    }
+}
